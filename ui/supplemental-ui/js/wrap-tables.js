7e649722ae1b475ed82fb87af61cd6d51461f58b
Bart Kleijngeld <bartkl@gmail.com>
2026-04-05 15:15:58 +0200

    Deleted supplemental UI files

--- ui/supplemental-ui/js/wrap-tables.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll("table.tableblock").forEach( function (table) {
-    var container = Object.assign(document.createElement("div"), { className: "table-container" })
-    table.parentNode.insertBefore(container, table).appendChild(table)
-})
